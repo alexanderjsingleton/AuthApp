b2a23242e96dc0eeddc1b4f9cfef9ae4c3497274
alexanderjsingleton <alexanderjsingleton@gmail.com>
2019-06-07 15:07:44 -0500

    added npm passport and conditional request parameters for user-auth

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
 
 const port = process.env.PORT || 3000;
 app.listen(port , () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
